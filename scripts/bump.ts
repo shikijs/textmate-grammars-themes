27f715f3fed3aaffa90e1d2acd8ad1f49402e94f
Anthony Fu <anthonyfu117@hotmail.com>
2023-12-20 17:48:40 +0100

    feat: auto bump and publish

---
+++ scripts/bump.ts
@@ -0,0 +1,40 @@
+import fs from 'node:fs/promises'
+import process from 'node:process'
+import Git from 'simple-git'
+import semver from 'semver'
+
+const git = Git()
+
+await git.add(['.'])
+
+const result = await git.status()
+
+const files = result.files.map(i => i.path)
+
+const grammarsChanged = files.some(i => i.startsWith('packages/tm-grammars/grammars'))
+const themesChanged = files.some(i => i.startsWith('packages/tm-themes/themes'))
+
+async function bumpVersion(path: string) {
+  const raw = await fs.readFile(path, 'utf-8')
+  const json = JSON.parse(raw)
+  const version = json.version.split('.')
+  json.version = semver.inc(version, 'patch')
+  await fs.writeFile(path, `${JSON.stringify(json, null, 2)}\n`, 'utf-8')
+}
+
+if (grammarsChanged) {
+  console.log('Grammars changed, bumping version...')
+  await bumpVersion('packages/tm-grammars/package.json')
+}
+
+if (themesChanged) {
+  console.log('Themes changed, bumping version...')
+  await bumpVersion('packages/tm-themes/package.json')
+}
+
+if ((grammarsChanged || themesChanged) && process.env.GITHUB_ENV) {
+  console.log('Grammars or themes changed, setting TM_CHANAGED=true...', process.env.GITHUB_ENV)
+  await fs.writeFile(process.env.GITHUB_ENV, `TM_CHANAGED=true`, 'utf-8')
+}
+
+await git.add(['.'])
