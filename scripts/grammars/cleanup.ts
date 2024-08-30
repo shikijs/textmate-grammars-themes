5f49bb7f6af020d8fe198255c35fdf372d350e35
Anthony Fu <github@antfu.me>
2024-08-30 11:53:16 +0200

    chore: extract logic to lib

--- scripts/grammars/cleanup.ts
+++ scripts/grammars/cleanup.ts
@@ -1,13 +1,30 @@
+import { syntaxLowering } from 'oniguruma-to-js'
+import { loweringTextmateGrammar } from 'oniguruma-to-js/textmate'
 import { objectPick } from '../shared/utils'
 import { highlight } from './highlight'
-import { regexpReprint } from './reprint'
 
-export async function cleanupGrammar(lang: any, verify = true) {
-  const before = verify ? await highlight(lang) : null
+export async function cleanupGrammar(grammar: any, verify = true) {
+  const before = verify ? await highlight(grammar) : null
 
-  lang = structuredClone(lang)
-  const picked = objectPick(
-    lang,
+  let converted = loweringTextmateGrammar(
+    grammar,
+    {
+      handlePattern(pattern) {
+        // https://github.com/shikijs/shiki/issues/591#issuecomment-1961637557
+        // It seems the `shellscript` grammars has a selector that missing the backslash escape, we patched them here
+        if (grammar.name === 'shellscript') {
+          pattern = pattern.replace(/\[\^ \t\n/g, '[^ \\t\\n')
+        }
+
+        // Rewrite Oniguruma patterns to more JS-friendly syntax
+        // Also it would "compress" the regex to save some bytes
+        return syntaxLowering(pattern)
+      },
+    },
+  )
+
+  converted = objectPick(
+    converted,
     [
       'aliases',
       'balancedBracketSelectors',
@@ -29,48 +46,11 @@ export async function cleanupGrammar(lang: any, verify = true) {
     // (_, key, value) => console.log('lang key removal', key, '|', value),
   )
 
-  function cleanupMatch(regex: string) {
-    // const original = regex
-
-    // https://github.com/shikijs/shiki/issues/591#issuecomment-1961637557
-    // It seems the `shellscript` grammars has a selector that missing the backslash escape, we patched them here
-    if (lang.name === 'shellscript')
-      regex = regex.replace(/\[\^ \t\n/g, '[^ \\t\\n')
-
-    regex = regexpReprint(regex)
-
-    // if (original !== regex) {
-    //   console.log({ original, regex })
-    // }
-
-    return regex
-  }
-
-  function cleanupPattern(a: any) {
-    if (a.match)
-      a.match = cleanupMatch(a.match)
-    if (a.begin)
-      a.begin = cleanupMatch(a.begin)
-    if (a.end)
-      a.end = cleanupMatch(a.end)
-    if (a.patterns) {
-      a.patterns.forEach((j: any) => {
-        cleanupPattern(j)
-      })
-    }
-  }
-
-  if (picked.patterns)
-    picked.patterns.forEach(cleanupPattern)
-  Object.values(picked.repository || {}).forEach((i: any) => {
-    cleanupPattern(i)
-  })
-
-  const after = verify ? await highlight(picked) : null
+  const after = verify ? await highlight(converted) : null
 
   if (verify && before !== after) {
-    throw new Error(`cleanup failed for ${lang.name}`)
+    throw new Error(`cleanup failed for ${grammar.name}`)
   }
 
-  return picked
+  return converted
 }
