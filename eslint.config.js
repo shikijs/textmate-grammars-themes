a8d973b6ea1cc4cbfb05130fc332948771c994e4
Anthony Fu <github@antfu.me>
2024-09-03 22:14:43 +0200

    feat: commit raw grammars

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,8 @@ export default antfu(
   {
     ignores: [
       'packages/tm-grammars/grammars/**',
+      'packages/tm-grammars/raw/**',
+      'packages/tm-themes/themes/**',
     ],
     vue: true,
   },
